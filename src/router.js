2fed52b7c261c1e3abf1893a0a763e0343aa5938
Venipa <admin@venipa.net>
2020-11-25 21:29:07 +0100

    prod route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/HomePage.vue";
-const history = createWebHistory();
+const history = createWebHistory(process.env.NODE_ENV === 'production' ? '/venipa-archive-browser/' : '/');
 const routes = [
   { path: "/", component: Home }
 ];
